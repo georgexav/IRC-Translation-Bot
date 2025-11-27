71f4a07d444be7fc0e753158956a1e4c50f3bf57
George Xavier <g3orgexav@gmail.com>
2025-11-27 15:43:56 -0500

    Changed version number to 3.0 in AboutCommand.py

--- com/georgex/trbot/command/AboutCommand.py
+++ com/georgex/trbot/command/AboutCommand.py
@@ -13,7 +13,7 @@ class AboutCommand(AbstractBotCommand):
         target_channel = self.app_config.get_target_channel() if self.app_config.get_target_channel().startswith('#') else '#' + self.app_config.get_target_channel()
         lang_code = self.app_config.get_target_language()
         language = langcodes.Language.make(lang_code).display_name()
-        responses.append(f'{nick} is a translation bot developed by asi9. The current version is 1.0 and was released 2025-11-27.')
+        responses.append(f'{nick} is a translation bot developed by asi9. The current version is 3.0 and was released 2025-11-27.')
         responses.append(f'Messages received in {source_channel} are translated to {language} when they are in another language.')
         responses.append(f'Translated messages are sent to {target_channel}.')
         return responses
